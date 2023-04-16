5a1c98f02473ec2f6da15c4fe10885bc76ea7ab8
Kate Komleva <komlevakate99@gmail.com>
2023-04-16 12:21:28 +0300

    Initial commit

---
+++ src/test/java/com/ugive/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ugive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
