8dac7a0842b6a72d644bbf00d5bcaa1e33f2d619
Kate Komleva <komlevakate99@gmail.com>
2023-05-10 22:25:27 +0300

    feat: user service

--- common/src/main/java/com/ugive/services/UserService.java
+++ common/src/main/java/com/ugive/services/UserService.java
@@ -9,9 +9,9 @@ import java.util.Optional;
 public interface UserService {
     Optional<User> create(UserDto userDto);
     Optional<User> update(Long id, UserDto userDto);
-    public Optional<User> setUserRole(Long userId, String roleName);
-    public List<User> findAll(int page, int size);
-    public User findOne(Long id);
-    public void softDelete(Long id);
-    public Optional<User> resetAccount(Long id);
+    Optional<User> setUserRole(Long userId, String roleName);
+    List<User> findAll(int page, int size);
+    User findOne(Long id);
+    void softDelete(Long id);
+    Optional<User> resetAccount(Long id);
 }
