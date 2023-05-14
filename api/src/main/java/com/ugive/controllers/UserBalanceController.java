f17ec607a50606fe4f76adf68ef3453f821f1302
Kate Komleva <komlevakate99@gmail.com>
2023-05-14 19:49:07 +0300

    feat: add user balance repository, controller.

--- api/src/main/java/com/ugive/controllers/UserBalanceController.java
+++ api/src/main/java/com/ugive/controllers/UserBalanceController.java
@@ -1,32 +1,80 @@
 package com.ugive.controllers;
 
+import com.ugive.dto.UserBalanceDto;
 import com.ugive.models.UserBalance;
 import com.ugive.repositories.UserBalanceRepository;
+import com.ugive.services.UserBalanceService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.math.BigDecimal;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
-@RequestMapping("/balance")
+@RequestMapping("/users")
 @RequiredArgsConstructor
 public class UserBalanceController {
-    private final UserBalanceRepository userBalanceRepository;
+    private final UserBalanceService userBalanceService;
 
-    @GetMapping
-    public ResponseEntity<Object> getAllUsersBalances() {
-        List<UserBalance> balances = userBalanceRepository.findAll();
-        return new ResponseEntity<>(balances, HttpStatus.OK);
+    @GetMapping("/balance")
+    public ResponseEntity<List<UserBalanceDto>> findAll(
+            @RequestParam(value = "page", defaultValue = "0") int page,
+            @RequestParam(value = "size", defaultValue = "10") int size) {
+        List<UserBalanceDto> userBalances = userBalanceService.findAll(page, size);
+        return new ResponseEntity<>(userBalances, HttpStatus.OK);
     }
 
-    @GetMapping("/{id}")
-    public ResponseEntity<Object> getBalanceById(@PathVariable Long id) {
-        UserBalance userBalance = userBalanceRepository.findByUserId(id);
-        return  new ResponseEntity<>(userBalance, HttpStatus.OK);
+    @GetMapping("/balance/{id}")
+    public ResponseEntity<UserBalanceDto> getBalanceById(@PathVariable Long id) {
+        return ResponseEntity.ok(userBalanceService.findOne(id));
+    }
+
+    @GetMapping("{userId}/balance")
+    public ResponseEntity<UserBalanceDto> getBalanceByUserId(@PathVariable Long userId) {
+        return ResponseEntity.ok(userBalanceService.getBalanceByUserId(userId));
+    }
+
+    @PostMapping("/balance/create")
+    public ResponseEntity<Optional<UserBalance>> createUserBalance(@Valid @RequestBody UserBalanceDto userBalanceDto) {
+        Optional<UserBalance> userBalance = userBalanceService.create(userBalanceDto);
+        return new ResponseEntity<>(userBalance, HttpStatus.CREATED);
+    }
+
+    @PutMapping("/balance/{id}/update")
+    public ResponseEntity<Optional<UserBalance>> updateBalance(@PathVariable("id") Long id, @RequestBody UserBalanceDto userBalanceDto) {
+        Optional<UserBalance> userBalance = userBalanceService.update(id, userBalanceDto);
+        return new ResponseEntity<>(userBalance, HttpStatus.CREATED);
+    }
+
+    @PutMapping("/balance/{id}/deposit")
+    public ResponseEntity<UserBalanceDto> putMoneyOnBalance(@PathVariable("id") Long id, @RequestParam BigDecimal newMoney) {
+        UserBalanceDto userBalance = userBalanceService.topUpBalance(id, newMoney);
+        return new ResponseEntity<>(userBalance, HttpStatus.CREATED);
+    }
+
+    @PutMapping("/balance/{id}/send")
+    public ResponseEntity<UserBalanceDto> putMoneyOnBalance(@PathVariable("id") Long idFirstUserBalance,
+                                                            @RequestParam Long idSecondUserBalance,
+                                                            @RequestParam BigDecimal newMoney) {
+        UserBalanceDto userBalance = userBalanceService.sendMoney(idFirstUserBalance, idSecondUserBalance, newMoney);
+        return new ResponseEntity<>(userBalance, HttpStatus.CREATED);
+    }
+
+    @DeleteMapping("/balance/{id}/delete")
+    public ResponseEntity<String> deleteBalance(@PathVariable("id") Long id) {
+        userBalanceService.softDelete(id);
+        return new ResponseEntity<>("This balance is deleted.", HttpStatus.OK);
     }
 }
--- common/src/main/java/com/ugive/repositories/UserBalanceRepository.java
+++ common/src/main/java/com/ugive/repositories/UserBalanceRepository.java
@@ -2,12 +2,21 @@ package com.ugive.repositories;
 
 import com.ugive.models.UserBalance;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
+
+import java.sql.Timestamp;
 
 public interface UserBalanceRepository extends
         JpaRepository<UserBalance, Long>,
         PagingAndSortingRepository<UserBalance, Long>,
         CrudRepository<UserBalance, Long> {
     UserBalance findByUserId(Long id);
+
+    @Modifying
+    @Query("DELETE FROM UserBalance ub WHERE ub.isDeleted = true AND ub.changed < :expirationDate")
+    void deleteExpiredUserBalance(@Param("expirationDate") Timestamp expirationDate);
 }
