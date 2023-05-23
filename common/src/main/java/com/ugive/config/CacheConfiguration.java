1a15276ebbf1dcc6f99fa36ba14d90d55258d0c1
Kate Komleva <komlevakate99@gmail.com>
2023-05-23 12:22:53 +0300

    feat: add cache to application.yml.

--- common/src/main/java/com/ugive/config/CacheConfiguration.java
+++ common/src/main/java/com/ugive/config/CacheConfiguration.java
@@ -1,15 +1,26 @@
 package com.ugive.config;
 
 import com.github.benmanes.caffeine.cache.Caffeine;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.caffeine.CaffeineCacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 
 import java.util.concurrent.TimeUnit;
 
+@Setter
+@Getter
 @Configuration
+@ConfigurationProperties(prefix = "caffeine")
+@PropertySource(value = "classpath:application.yml")
 public class CacheConfiguration {
+    private Integer initialCapacity;
+    private Integer maximumSize;
+    private Integer expireAfterAccessHours;
     @Bean
     public CacheManager cacheManager() {
         CaffeineCacheManager cacheManager = new CaffeineCacheManager("c_offer_status", "c_payment_type",
@@ -20,9 +31,9 @@ public class CacheConfiguration {
 
     public Caffeine<Object, Object> cacheProperties() {
         return Caffeine.newBuilder()
-                .initialCapacity(30)
-                .maximumSize(2000)
-                .expireAfterAccess(60, TimeUnit.MINUTES)
+                .initialCapacity(initialCapacity)
+                .maximumSize(maximumSize)
+                .expireAfterAccess(expireAfterAccessHours, TimeUnit.HOURS)
                 .weakKeys()
                 .recordStats();
     }
